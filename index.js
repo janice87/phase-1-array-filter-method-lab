340b95b4494f28a97350b8508571945310ef8f01
Janice Chan <janice.chan1288@gmail.com>
2022-03-18 06:41:25 -0400

    commit tree 37e4969c28b1ebbfa27a62ba569c3284c69af3c9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = function(driversArray, driver) {
+    return driversArray.filter(item => item === driver || item === driver.toLowerCase())
+}
+
+const fuzzyMatch = function(driversArray, driver) {
+    return driversArray.filter(item => item[0] === driver[0] && item[1] === driver[1])
+        }
+
+
+const matchName = function(driversArrayObj, driver) {
+ let match = driversArrayObj.filter(item => item.name === driver) 
+     return match
+    }
